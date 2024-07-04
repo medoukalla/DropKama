a4893128b5dfe48f85ffe4593ce13cb3c836e313
Oukalla Mohamed <medoukalla@gmail.com>
2024-07-04 23:32:29 +0100

    Update offers table in database & Update database

--- database/migrations/2024_06_27_1719484314_create_offers_table.php
+++ database/migrations/2024_06_27_1719484314_create_offers_table.php
@@ -17,16 +17,20 @@ class CreateOffersTable extends Migration
 		$table->decimal('quantity',10,3)->nullable();
 		$table->decimal('total',8,3)->nullable();
 		$table->string('game_id')->nullable();
-		$table->string('payment')->nullable();
+		$table->bigInteger('payment_id',)->unsigned();
 		$table->string('payment_info')->nullable();
+		$table->string('payment_info_b')->nullable();
 		$table->string('name');
 		$table->string('email');
 		$table->string('discord');
-		$table->enum('status',['en cours','paye','annule'])->default('en cours');
+		$table->enum('status',['pending','progress','cancelled','completed'])->default('pending');
 		
 		$table->timestamps();
 		
-		$table->foreign('server_id')->references('id')->on('offer_servers');		$table->foreign('user_id')->references('id')->on('users');
+		// $table->foreign('server_id')->references('id')->on('offer_servers');		
+		// $table->foreign('user_id')->references('id')->on('users');
+		// $table->foreign('payment_id')->references('id')->on('payments');
+
         });
     }
 
