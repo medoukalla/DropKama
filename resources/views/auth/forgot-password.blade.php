2ff7621967e1aeddfa068327f1840eea4cd019a5
Oukalla Mohamed <medoukalla@gmail.com>
2024-07-05 17:24:20 +0100

    Finish forgot password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,35 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<x-frontend.head />
+<div class="authentication login-section">
+    <div class="auth-body">
+        <form method="POST" action="{{ route('password.email') }}" >
+            @csrf
+            <div class="auth-section">
+                <div class="auth-logo">
+                    <a href="{{ route('frontend.index') }}">
+                        <img src="{{ asset('frontend/images/logo.png') }}" alt="" />
+                    </a>
+                </div>
+                <div class="auth-heading">Mot de passe oublié?</div>
+                <p class="text-light">
+                    Aucun problème. Indiquez-nous simplement votre adresse e-mail et nous vous enverrons par e-mail un lien de réinitialisation de mot de passe qui vous permettra d'en choisir un nouveau.
+                </p>
+                <div class="auth-inputs">
+                    <div class="email">
+                        <label for="">{{ __('Email') }}</label>
+                        <input type="email" id="email" name="email" :value="old('email')" required autofocus class="mt-2 mb-3" />
+                    </div>
+                </div>
+                <div class="alert alert-danger w-100 @if ( $errors->count() == 0 ) d-none @endif ">
+                    {{ $errors->first() }}
+                </div>
+                <button class="main-btn px-0 py-4 w-50" role="button" type="submit">
+                    {{ __('Email Password Reset Link') }}
+                </button>
+                
+                <a href="{{ route('login') }}">
+                    <p class="create-acc">Connectez-vous !</p>
+                </a>
+            </div>
+        </form>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</div>
