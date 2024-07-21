272fcbc32afd534daeffbcaf6b307a43f0af6053
Oukalla Mohamed <medoukalla@gmail.com>
2024-07-21 22:30:25 +0100

    Finish the reset password last page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,60 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<x-frontend.head />
+<div class="authentication login-section">
+    <div class="auth-body">
+        <form method="POST" action="{{ route('password.store') }}" >
+            @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="auth-section">
+                <div class="auth-logo">
+                    <a href="{{ route('frontend.index') }}">
+                        <img src="{{ asset('frontend/images/logo.png') }}" alt="" />
+                    </a>
+                </div>
+                <div class="auth-heading">Créer un nouveau mot de passe !</div>
+                
+                
+                <div class="auth-inputs">
+                    <div class="email">
+                    <x-input-label for="email"  />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
+                    </div>
+                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <div class="auth-inputs">
+                    <div class="email">
+                    <x-input-label for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
+                    </div>
+                </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+                <div class="auth-inputs">
+                    <div class="email">
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password_confirmation" required autocomplete="new-password" />
+        
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
+                    </div>
+                </div>
+                
+                
+                <button class="main-btn px-0 py-4 w-50" role="button" type="submit">
+                    {{ __('Reset Password') }}
+                </button>
+                
+                <a href="{{ route('login') }}">
+                    <p class="create-acc">Connectez-vous !</p>
+                </a>
+            </div>
+        </form>
+    </div>
+</div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
