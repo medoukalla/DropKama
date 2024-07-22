73608556e01e7ca4469332cbb238c9fe97d2acb7
marwanbenz <benzsram@gmail.com>
2024-07-22 15:49:38 +0100

    Policies pages fixed header

--- resources/views/frontend/politique.blade.php
+++ resources/views/frontend/politique.blade.php
@@ -1,6 +1,32 @@
 <x-frontend.head />
 
-<x-frontend.min-header message='{{ $message }}' title="{{ $title }}" />
+<!-- Hero section -->
+<div class="shine-light">
+    <img src="{{ asset('frontend/images/svg/shine-light.svg') }}" alt="Shine light" />
+</div>
+<header class="second-header">
+    <div class="container">
+        <div class="hero-section">
+
+            <!-- Mobile menu -->
+            <x-frontend.mobile-menu />
+
+            <!-- Desktop menu -->
+            <x-frontend.desktop-menu />
+
+
+            <div class="row hero-banner align-items-star" style="z-index: 100">
+                <div class="col-12 col-md-12">
+                    <div class="page-title text-center">
+                        {{ $title }}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+<!-- End section -->
 
 
 <section class="acheter-processing bg-not-white">
@@ -9,11 +35,11 @@
         <div class="row">
             <div class="col-12 ">
                 <div class="kamas-settings w-100 sticky-top">
-                    
-                    
+
+
                     {!! setting('site.politique') !!}
 
-    
+
                 </div>
             </div>
         </div>
