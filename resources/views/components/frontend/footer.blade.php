3d0ad3445fe731403764239b9585968747dbb804
Oukalla Mohamed <medoukalla@gmail.com>
2024-07-22 18:45:05 +0100

    Add tawk chat

--- resources/views/components/frontend/footer.blade.php
+++ resources/views/components/frontend/footer.blade.php
@@ -117,6 +117,19 @@
   integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
   crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/669e98e6becc2fed692906f3/1i3doahae';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
 @livewireScripts
 </body>
 
