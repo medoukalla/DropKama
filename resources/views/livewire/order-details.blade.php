6918328aac8d796ba80e74a71ff3169c8c899bb0
marwanbenz <benzsram@gmail.com>
2024-07-29 16:46:08 +0100

    Design bugs update

--- resources/views/livewire/order-details.blade.php
+++ resources/views/livewire/order-details.blade.php
@@ -5,13 +5,15 @@
             <div class="col-12 col-md-6">
                 <div class="product-line bg-component-white sticky-top">
                     <div class="line">
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif active">
+                        <div class="h-step @if (!is_null($order->deleted_at)) rejected @endif active">
                             <div class="i-step">
-                                
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
-                                    <img src="{{ asset('frontend/images/svg/checked.svg') }}" alt="" height="30" class="ready">
+
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
+                                    <img src="{{ asset('frontend/images/svg/checked.svg') }}" alt=""
+                                        height="30" class="ready">
                                 @endif
 
                             </div>
@@ -19,11 +21,13 @@
                                 Traitement du paiement en attente
                             </div>
                         </div>
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif @if ($order->payed == true && $order->payment_verified == true) active @endif">
+                        <div
+                            class="h-step @if (!is_null($order->deleted_at)) rejected @endif @if ($order->payed == true && $order->payment_verified == true) active @endif">
                             <div class="i-step">
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
                                     @if ($order->payed == true && $order->payment_verified == false)
                                         <img src="{{ asset('frontend/images/svg/not-ready.svg') }}" alt=""
                                             height="30" class="not-ready">
@@ -37,11 +41,13 @@
                                 Confirmation du paiement
                             </div>
                         </div>
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif @if ($order->payment_verified == true && $order->facturer == true) active @endif">
+                        <div
+                            class="h-step @if (!is_null($order->deleted_at)) rejected @endif @if ($order->payment_verified == true && $order->facturer == true) active @endif">
                             <div class="i-step">
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
                                     @if (($order->payment_verified == true && $order->facturer == false) || $order->payment_verified == false)
                                         <img src="{{ asset('frontend/images/svg/not-ready.svg') }}" alt=""
                                             height="30" class="not-ready">
@@ -55,11 +61,13 @@
                                 Coordonnées de facturation
                             </div>
                         </div>
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif @if ($order->facturer == true && $order->liviser == true) active @endif">
+                        <div
+                            class="h-step @if (!is_null($order->deleted_at)) rejected @endif @if ($order->facturer == true && $order->liviser == true) active @endif">
                             <div class="i-step">
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
                                     @if (($order->facturer == true && $order->liviser == false) || $order->facturer == false)
                                         <img src="{{ asset('frontend/images/svg/not-ready.svg') }}" alt=""
                                             height="30" class="not-ready">
@@ -73,11 +81,13 @@
                                 Détails de livraison
                             </div>
                         </div>
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif @if ($order->liviser == true && $order->delivered == true) active @endif">
+                        <div
+                            class="h-step @if (!is_null($order->deleted_at)) rejected @endif @if ($order->liviser == true && $order->delivered == true) active @endif">
                             <div class="i-step">
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
                                     @if (($order->liviser == true && $order->delivered == false) || $order->liviser == false)
                                         <img src="{{ asset('frontend/images/svg/not-ready.svg') }}" alt=""
                                             height="30" class="not-ready">
@@ -91,11 +101,13 @@
                                 Livraison en cours
                             </div>
                         </div>
-                        <div class="h-step @if ( !is_null($order->deleted_at) ) rejected @endif @if ($order->delivered == true) active @endif">
+                        <div
+                            class="h-step @if (!is_null($order->deleted_at)) rejected @endif @if ($order->delivered == true) active @endif">
                             <div class="i-step">
-                                @if ( !is_null( $order->deleted_at ) )
-                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt="" height="30" class="rejecting">
-                                @else 
+                                @if (!is_null($order->deleted_at))
+                                    <img src="{{ asset('frontend/images/svg/reject-icon.svg') }}" alt=""
+                                        height="30" class="rejecting">
+                                @else
                                     @if ($order->delivered == false)
                                         <img src="{{ asset('frontend/images/svg/not-ready.svg') }}" alt=""
                                             height="30" class="not-ready">
@@ -116,7 +128,8 @@
             <div class="col-12 col-md-6 d-flex flex-column">
                 <!-- OPEN ------------------------>
                 <!-- Vérification du paiement Head -->
-                <div class="bg-component-white mb-3 @if ( !is_null( $order->deleted_at ) ) lock-step @endif @if ($order->payed == true && $order->payment_verified == false) @else lock-step @endif">
+                <div
+                    class="bg-component-white mb-3 @if (!is_null($order->deleted_at)) lock-step @endif @if ($order->payed == true && $order->payment_verified == false) @else lock-step @endif">
                     <div class="d-flex align-items-center justify-content-center gap-3">
                         <img src="{{ asset('frontend/images/svg/payment-verify.svg') }}" alt=""
                             height="30" />
@@ -127,7 +140,8 @@
               </a> -->
                 </div>
                 <!-- Question -->
-                <div class="bg-component-white mb-3 @if ( !is_null( $order->deleted_at ) ) d-none @endif @if ($order->payed == true && $order->payment_verified == false || !is_null( $order->deleted_at )) @else d-none @endif">
+                <div
+                    class="bg-component-white mb-3 @if (!is_null($order->deleted_at)) d-none @endif @if (($order->payed == true && $order->payment_verified == false) || !is_null($order->deleted_at)) @else d-none @endif">
                     <p class="d-flex align-items-start gap-3">
                         Nous avons enregistré votre paiement pour cette commande. Pour
                         vérification, veuillez communiquer votre numéro de commande,
@@ -141,9 +155,10 @@
 
                 <!-- Coordonnées de facturation Head-->
                 <div
-                    class="step-heading bg-component-white mb-3 @if ( !is_null( $order->deleted_at ) ) lock-step @endif @if ($order->payment_verified == true && $order->facturer == false ) @else lock-step @endif">
+                    class="step-heading bg-component-white mb-3 @if (!is_null($order->deleted_at)) lock-step @endif @if ($order->payment_verified == true && $order->facturer == false) @else lock-step @endif">
                     <div class="d-flex align-items-center justify-content-center gap-3">
-                        <img src="{{ asset('frontend/images/svg/facturation.svg') }}" alt="" height="30" />
+                        <img src="{{ asset('frontend/images/svg/facturation.svg') }}" alt=""
+                            height="30" />
                         <h5>Coordonnées de facturation</h5>
                     </div>
                     <!-- <a href="#" class="">
@@ -153,7 +168,7 @@
 
                 <!-- Coordonnées de facturation inputs -->
                 <div
-                    class="step-inputs bg-component-white mb-3 kamas-settings w-100 @if ( !is_null( $order->deleted_at ) ) d-none @endif @if ($order->payment_verified == true && $order->facturer == false) @else d-none @endif">
+                    class="step-inputs bg-component-white mb-3 kamas-settings w-100 @if (!is_null($order->deleted_at)) d-none @endif @if ($order->payment_verified == true && $order->facturer == false) @else d-none @endif">
 
 
                     <div class="input mt-0 @if ($facturation_steps > 2) d-none @endif">
@@ -162,8 +177,8 @@
                             <h6>Lorem ipsum dolor sit amet.</h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-between">
-                            <input wire:model.defer="email" type="email" id="normalInput" value="youssef@gmail.com"
-                                disabled />
+                            <input wire:model.defer="email" type="email" id="normalInput"
+                                value="youssef@gmail.com" disabled />
                         </div>
                     </div>
 
@@ -254,7 +269,7 @@
 
                 <!-- Information de facturation head -->
                 <div
-                    class="step-heading bg-component-white mb-3 @if ( !is_null( $order->deleted_at ) ) lock-step @endif @if ($order->facturer == true && $order->liviser == false) @else lock-step @endif">
+                    class="step-heading bg-component-white mb-3 @if (!is_null($order->deleted_at)) lock-step @endif @if ($order->facturer == true && $order->liviser == false) @else lock-step @endif">
                     <div class="d-flex align-items-center justify-content-center gap-3">
                         <img src="{{ asset('frontend/images/svg/infos.svg') }}" alt="" height="30" />
                         <h5>Détails de livraison</h5>
@@ -262,7 +277,7 @@
                 </div>
                 <!-- Information de facturation inputs -->
                 <div
-                    class="step-inputs bg-component-white mb-3 kamas-settings w-100 @if ( !is_null( $order->deleted_at ) ) d-none @endif  @if ($order->facturer == true && $order->liviser == false) @else d-none @endif">
+                    class="step-inputs bg-component-white mb-3 kamas-settings w-100 @if (!is_null($order->deleted_at)) d-none @endif  @if ($order->facturer == true && $order->liviser == false) @else d-none @endif">
                     <div class="infos bg-dark-white mb-3 d-flex align-items-center justify-content-between">
                         <div>Nom de personnage : </div>
                         <div>{{ $order->user->username }}</div>
@@ -292,34 +307,38 @@
                 </div>
                 <!-- Détails de la commande inputs -->
                 <div class="step-inputs bg-component-white">
-                    @if ( !is_null( $order->deleted_at ) )
+                    @if (!is_null($order->deleted_at))
                         <div class="alert alert-danger ">
-                            Nous regrettons de vous informer que votre commande récente a été rejetée. N'hésitez pas à passer une nouvelle commande à votre convenance. Si vous avez besoin d'aide, nous sommes là pour vous assister.
+                            Nous regrettons de vous informer que votre commande récente a été rejetée. N'hésitez pas à
+                            passer une nouvelle commande à votre convenance. Si vous avez besoin d'aide, nous sommes là
+                            pour vous assister.
                         </div>
                     @elseif($order->liviser == true && $order->order_completed == false)
                         <div class="alert alert-success ">
                             S'il vous plaît, attendez ! nous vous contacterons dans moins de 5 minutes. <br>
                             Si cela prend plus que cela, veuillez nous contacter dans le chat ci-dessous, merci
                         </div>
-                    @elseif ( $order->order_completed == true )
+                    @elseif ($order->order_completed == true)
                         <div class="alert alert-success ">
-                            Nous avons le plaisir de vous informer que votre commande a été remplie et complétée avec succès. Merci d'avoir choisi nos services. Si vous avez des questions ou besoin d'aide supplémentaire, n'hésitez pas à nous contacter.
+                            Nous avons le plaisir de vous informer que votre commande a été remplie et complétée avec
+                            succès. Merci d'avoir choisi nos services. Si vous avez des questions ou besoin d'aide
+                            supplémentaire, n'hésitez pas à nous contacter.
                         </div>
                     @endif
-                    <div class="d-flex align-items-center justify-content-between mb-3 gap-3">
+                    <div class="d-flex align-items-center justify-content-between mb-3 gap-3 imp-components">
                         <div class="info-component">{{ $order->server->name }} {{ $order->quantity * 1 }} M pour :
                             {{ $order->user->username }}</div>
                         <div class="info-component">
                             {{ $order->total }} {{ strtoupper(Session::get('currency')) }}
                         </div>
                     </div>
-                    <div class="d-flex align-items-center justify-content-between mb-3 gap-3">
+                    <div class="d-flex align-items-center justify-content-between mb-3 gap-3 imp-components">
                         <div class="info-component">Référence</div>
                         <div class="info-component">
                             {{ $order->reference }}
                         </div>
                     </div>
-                    <div class="d-flex align-items-center justify-content-between">
+                    <div class="d-flex align-items-center justify-content-between gap-3 imp-components">
                         <div class="info-component bg-orange">Total</div>
                         <div class="info-component bg-orange">
                             {{ $order->total }} {{ strtoupper(Session::get('currency')) }}
